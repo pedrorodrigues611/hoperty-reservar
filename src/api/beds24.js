a34953a883f0d5cede0d23ec3e6914403a2cfa2a
pedrorodrigues611 <hopertybnb@gmail.com>
2025-04-15 17:04:15 +0100

    Delete src directory

--- src/api/beds24.js
+++
@@ -1,6 +0,0 @@
-export async function fetchProperties({ location, guests, checkin, checkout }) {
-  const query = new URLSearchParams({ location, guests, checkin, checkout });
-  const response = await fetch(`/api/reservas?${query.toString()}`);
-  const data = await response.json();
-  return data;
-}
