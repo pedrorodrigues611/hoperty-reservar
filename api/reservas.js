84a405227edbe795ad5aee856b0d0dc7e7ff7d8c
pedrorodrigues611 <hopertybnb@gmail.com>
2025-04-15 17:39:16 +0100

    Update reservas.js

--- api/reservas.js
+++ api/reservas.js
@@ -1,21 +1,21 @@
 export default async function handler(req, res) {
-  const { location, guests, checkin, checkout } = req.query;
+  const apiKey = process.env.VITE_BEDS24_API_KEY;
 
   const response = await fetch("https://api.beds24.com/json/getProperties", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({
-      authentication: { apiKey: process.env.BEDS24_API_KEY },
+      authentication: {
+        apiKey: apiKey
+      },
       parameters: {
-        location,
-        guests,
-        checkin,
-        checkout,
         includeRooms: true
       }
     })
   });
 
   const data = await response.json();
-  res.status(200).json(data.properties || []);
+  res.status(200).json(data);
 }
